ce8180f53d5e6dc9ef33bef95c1ce3c46703d2e6
unknown <emilbrankov444@gmail.com>
2021-01-27 15:10:22 +0200

    Add routes and create page

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const config = require('./config/config')
+const config = require('./config/config');
+const routes = require('./routes');
 const expressConfig = require('./config/express');
 
 const app = express();
-
 expressConfig(app);
 
-
-app.get('/', (req, res) => {
-
-    res.render('home', {layout: false})
-})
+app.use(routes);
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
